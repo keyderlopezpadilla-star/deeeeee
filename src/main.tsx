fe660325a0c7238b274505e32272ec5d537afe3e
keyderlopezpadilla-star <keyderlopezpadilla@gmail.com>
2026-06-19 13:02:20 +0200

    fix: remove BrowserRouter wrapper to restore app render

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
-const basename = import.meta.env.BASE_URL
-
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={basename}>
-    <App />
-  </BrowserRouter>,
+  <App />,
 )
